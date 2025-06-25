ce88a6a937efd7859da75d677da36ee187ee3013
dhanshri <dhanshriwanjari4009@gmail.com>
2025-06-25 16:16:12 +0530

    commit

--- README.md
+++
@@ -1,1 +0,0 @@
-A real-time AI-powered study assistant that detects your emotions via webcam and gives personalized help to improve focus and learning
--- my-loginpage-backend/index.js
+++
@@ -1,8 +0,0 @@
-// Load Firebase Admin
-const admin = require('./firebaseAdmin');
-
-// Example usage: verify Firebase ID token (if needed)
-admin.auth().listUsers().then((list) => {
-  console.log("Total users:", list.users.length);
-});
-
